d139839d763e3e9fd4edcaea6ab85c220e9800d4
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2024-05-14 13:57:03 -0400

    Fix display behavior

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let numA = 1;
-let numB = 2;
-let operator= 'add';
+var firstNumber;
+var secondNumber;
+var operator= '';
 
 function add(n1, n2) {
     return n1 + n2;
@@ -36,14 +36,32 @@ function operate(n1, n2, op) {
 }
 
 let display = document.querySelector('#display');
+display.textContent = "0";
+var displayValue = display.textContent;
 
-let keys = document.querySelectorAll('.key');
-
-keys.forEach(keys => keys.addEventListener('click', () => updateDisplay(keys.textContent)));
-
-var displayValue = "";
+let numberKeys = document.querySelectorAll('.number');
+numberKeys.forEach(key => key.addEventListener('click', () => updateDisplay(key.textContent)));
 
 function updateDisplay(n) {
-    displayValue += n;
-    display.textContent = displayValue;
+    if (displayValue == 0) {
+        displayValue = n;
+        display.textContent = displayValue;
+        return;
+    }
+    if (display.textContent.length < 8) {
+        displayValue += n;
+        display.textContent = displayValue;
+    }
+}
+
+let opKeys = document.querySelectorAll('.op');
+opKeys.forEach(opKeys => opKeys.addEventListener('click', () => updateDisplay(keys.textContent)));
+
+
+
+let clear = document.querySelectorAll('#keyClear');
+clear.forEach(clear => clear.addEventListener('click', () => clearAll()));
+function clearAll() {
+    displayValue = "0";
+    display.textContent = "0";
 }
