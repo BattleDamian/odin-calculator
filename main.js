c5b70249bf759002b0bad3e735f073223e5eb708
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2024-05-15 15:54:20 -0400

    quick commit... sry me

--- main.js
+++ main.js
@@ -2,36 +2,62 @@ var firstNumber;
 var secondNumber;
 var operator= '';
 
-function add(n1, n2) {
-    return n1 + n2;
+function add(firstNumber, secondNumber) {
+     let answer = Number(firstNumber) + Number(secondNumber);
+     displayValue = String(answer);
+     display.textContent = String(displayValue);
 }
 
-function subtract(n1, n2) {
-    return n1 - n2;
+function subtract(firstNumber, secondNumber) {
+    let answer = Number(firstNumber) - Number(secondNumber);
+    displayValue = String(answer);
+    display.textContent = String(displayValue);
 }
 
-function multiply(n1, n2) {
-    return n1 * n2;
+function multiply(firstNumber, secondNumber) {
+    let answer = Number(firstNumber) * Number(secondNumber);
+    displayValue = String(answer);
+    display.textContent = String(displayValue);
 }
 
-function divide(n1, n2) {
-    return n1 / n2;
+function divide(firstNumber, secondNumber) {
+    let answer = Number(firstNumber) / Number(secondNumber);
+    displayValue = String(answer);
+    display.textContent = String(displayValue);
 }
 
-function operate(n1, n2, op) {
-    switch (op) {
-        case 'add':
-            add(n1, n2);
-            break;
-        case 'subtract':
-            subtract(n1, n2)
-            break;
-        case 'multiply':
-            multiply(n1, n2)
-            break;
-        case 'divide':
-            divide(n1, n2)
-            break;
+function operate(n) {
+    if (n != "=") {
+        operator = n;
+    }
+
+    if (firstNumber == undefined) {
+        firstNumber = displayValue;
+        displayValue = undefined;
+        display.textContent = firstNumber;
+        return;
+    }
+
+    if (firstNumber != undefined & secondNumber == undefined) {
+        secondNumber = displayValue;
+        let a;
+        switch (operator) {
+            case '+':
+                a = add(firstNumber, secondNumber);
+                break;
+            case '-':
+                a = subtract(firstNumber, secondNumber)
+                break;
+            case '*':
+                a = multiply(firstNumber, secondNumber)
+                break;
+            case '/':
+                a = divide(firstNumber, secondNumber)
+                break;
+        }
+        firstNumber= undefined;
+        secondNumber= undefined;
+        operator= '';
     }
 }
 
@@ -43,28 +69,32 @@ let numberKeys = document.querySelectorAll('.number');
 numberKeys.forEach(numberKey => numberKey.addEventListener('click', () => updateDisplay(numberKey.textContent)));
 
 function updateDisplay(n) {
-    if (displayValue == 0) {
-        displayValue = n;
-        display.textContent = displayValue;
+    if (displayValue == "0" || displayValue == undefined) {
+        displayValue = String(n);
+        display.textContent = String(displayValue);
         return;
     }
+
+    if (displayValue != "0" || displayValue != undefined) {
+        operate()
+        return;
+    }
+
     if (display.textContent.length < 8) {
-        displayValue += n;
-        display.textContent = displayValue;
+        displayValue += String(n);
+        display.textContent = String(displayValue);
     }
 }
-
-let opKeys = document.querySelectorAll('.op');
-opKeys.forEach(opKey => opKey.addEventListener('click', () => updateDisplay(opKey.textContent)));
-
-
 
 let clearKey = document.querySelector('#keyClear');
 clearKey.addEventListener('click', () => clear());
 function clear() {
-    firstNumber;
-    secondNumber;
+    firstNumber= undefined;
+    secondNumber= undefined;
     operator= '';
     displayValue = "0";
     display.textContent = "0";
 }
+
+let opKeys = document.querySelectorAll('.operator');
+opKeys.forEach(opKey => opKey.addEventListener('click', () => operate(opKey.textContent)));
