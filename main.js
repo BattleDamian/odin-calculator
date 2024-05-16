7c616b310700c4d82684d25c0cd661f0a07d5e75
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2024-05-16 16:33:01 -0400

    Restructure main.js (big commit, sorry)

--- main.js
+++ main.js
@@ -1,99 +1,114 @@
-const data = {
-    firstNumber: undefined,
-    secondNumber: undefined,
-    operator: "",
-};
-const display = document.querySelector('#display').textContent;
-const numberKeys = document.querySelectorAll('.number');
-const opKeys = document.querySelectorAll('.operator');
-const clearKey = document.querySelector('#keyClear');
+let previousNumber = '';
+let currentNumber = '';
+let operator = '';
+let result = '';
+let updateDisplay = false;
+let loopLastOperation = false;
+const display = document.querySelector('#display');
+const keys = document.querySelectorAll('.key');
 
-var displayValue = display;
-numberKeys.forEach(numberKey => numberKey.addEventListener('click', () => updateDisplay(numberKey.textContent)));
-opKeys.forEach(opKey => opKey.addEventListener('click', () => operate(opKey.textContent)));
-
-function add(firstNumber, secondNumber) {
-     let answer = Number(firstNumber) + Number(secondNumber);
-     displayValue = String(answer);
-     display = String(displayValue);
-}
-
-function subtract(firstNumber, secondNumber) {
-    let answer = Number(firstNumber) - Number(secondNumber);
-    displayValue = String(answer);
-    display = String(displayValue);
-}
-
-function multiply(firstNumber, secondNumber) {
-    let answer = Number(firstNumber) * Number(secondNumber);
-    displayValue = String(answer);
-    display = String(displayValue);
-}
-
-function divide(firstNumber, secondNumber) {
-    let answer = Number(firstNumber) / Number(secondNumber);
-    displayValue = String(answer);
-    display = String(displayValue);
-}
-
-function operate(n) {
-    if (n != "=") {
-        operator = n;
+keys.forEach((keys) => keys.addEventListener('click', () => {
+    const key = keys.textContent;
+    if (key === '=') {
+        operate(key);
+        updateDisplay = true;
+        loopLastOperation = true;
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        grabOperator(key);
+        updateDisplay = true;
+        loopLastOperation = false;
+    } else if (key >= '0' && key <= '9') {
+        update(key);
+    } else if (key === 'AC') {
+        clear();
     }
+}));
 
-    if (firstNumber == undefined) {
-        firstNumber = displayValue;
-        displayValue = undefined;
-        display = firstNumber;
-        return;
-    }
+function grabOperator(key) {
+    operator = key;
+    previousNumber = currentNumber;
+    currentNumber = display.textContent;
+}
 
-    if (firstNumber != undefined & secondNumber == undefined) {
-        secondNumber = displayValue;
-        let a;
-        switch (operator) {
-            case '+':
-                a = add(firstNumber, secondNumber);
-                break;
-            case '-':
-                a = subtract(firstNumber, secondNumber)
-                break;
-            case '*':
-                a = multiply(firstNumber, secondNumber)
-                break;
-            case '/':
-                a = divide(firstNumber, secondNumber)
-                break;
+function operate() {
+    if (currentNumber && previousNumber) {
+        if (!loopLastOperation) {
+            switch (operator) {
+                case '+':
+                    add(previousNumber, currentNumber);
+                    break;
+                case '-':
+                    subtract(previousNumber, currentNumber)
+                    break;
+                case '*':
+                    multiply(previousNumber, currentNumber)
+                    break;
+                case '/':
+                    divide(previousNumber, currentNumber)
+                    break;
+            }
+            previousNumber = currentNumber;
+            currentNumber = result;
+        } else if (loopLastOperation) {
+            switch (operator) {
+                case '+':
+                    add(result, previousNumber);
+                    break;
+                case '-':
+                    subtract(result, previousNumber)
+                    break;
+                case '*':
+                    multiply(result, previousNumber)
+                    break;
+                case '/':
+                    divide(result, previousNumber)
+                    break;
+            }
         }
-        firstNumber= undefined;
-        secondNumber= undefined;
-        operator= '';
+
     }
 }
 
-function updateDisplay(n) {
-    if (displayValue == "0" || displayValue == undefined) {
-        displayValue = String(n);
-        display = String(displayValue);
-        return;
-    }
-
-    if (displayValue != "0" || displayValue != undefined) {
-        operate()
-        return;
-    }
-
-    if (display.length < 8) {
-        displayValue += String(n);
-        display = String(displayValue);
+function update(key) {
+    if (display.textContent.length < 8) {
+         if (updateDisplay) {
+            currentNumber = '';
+            currentNumber += key;
+            display.textContent = currentNumber;
+            updateDisplay = false;
+        } else {
+            currentNumber += key;
+            display.textContent = currentNumber; 
+        }
     }
 }
 
-clearKey.addEventListener('click', () => clear());
 function clear() {
-    firstNumber= undefined;
-    secondNumber= undefined;
+    currentNumber = '';
+    previousNumber= '';
     operator= '';
-    displayValue = "0";
-    display = "0";
+    result = '';
+    display.textContent = '0';
 }
+
+function add(previousNumber, currentNumber) {
+     result = String(Number(previousNumber) + Number(currentNumber));
+     display.textContent = String(result);
+}
+
+function subtract(previousNumber, currentNumber) {
+    result = String(Number(previousNumber) - Number(currentNumber));
+    display.textContent = String(result);
+}
+
+function multiply(previousNumber, currentNumber) {
+    result = String(Number(previousNumber) * Number(currentNumber));
+    display.textContent = String(result);
+}
+
+function divide(previousNumber, currentNumber) {
+    result = String(Number(previousNumber) / Number(currentNumber));
+    display.textContent = String(result);
+}
+
+clear();
